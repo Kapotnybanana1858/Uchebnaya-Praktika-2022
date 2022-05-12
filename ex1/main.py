dc13e1d0aef97254ead73fe2e5047bffd92a775a
Kapotnybanana1858 <90246832+Kapotnybanana1858@users.noreply.github.com>
2022-05-12 11:18:14 +0300

    Update main.py

--- ex1/main.py
+++ ex1/main.py
@@ -22,9 +22,10 @@ k = (y1 - y2) / (x1 - x2)
 b = y2 - k*x2
 print(" y = %.2f*x + %.2f" % (k, b))
 y = k * x2o + b
-
+x = (y - b) / k
 if y1o < y < y2o:
     print("Вход")
+
 elif y2o < y < y1o:
     print("Выход")
 else:
@@ -32,9 +33,17 @@ else:
 
 # Создать полностью черное изображение
 img = np.zeros((512, 512, 3), np.uint8)
-cv.line(img,(x1,y1),(x2,y2),(0,0,255),2,cv.LINE_8)
+cv.line(img,(int(-b/k),0),(int((512 -b) / k),512),(0,0,255),2,cv.LINE_8)
+cv.line(img,(x1o,y1o),(x2o,y2o),(255,0,0),2, cv.LINE_8)
+if y1o < y < y2o:
+    cv.putText(img, 'Enter', (10, 100), cv.FONT_HERSHEY_SCRIPT_SIMPLEX, 3, (255, 255, 255), 2, cv.LINE_AA)
+elif y2o < y < y1o:
+    cv.putText(img, 'Exit', (10, 100), cv.FONT_HERSHEY_SCRIPT_SIMPLEX, 3, (255, 255, 255), 2, cv.LINE_AA)
+else:
+    cv.putText(img, 'NotCrossed', (10, 100), cv.FONT_HERSHEY_SCRIPT_SIMPLEX, 3, (255, 255, 255), 2, cv.LINE_AA)
 # Дисплей
 cv.imshow('line', img)
+cv.imshow('Text', img)
 cv.waitKey(0)
 # Закрыть окно
 cv.destroyAllWindows()
